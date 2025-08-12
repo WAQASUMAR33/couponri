bb53b35942c660691047e599b1dafc65182f91c4
Waqas Umar <55554486+WAQASUMAR33@users.noreply.github.com>
2025-08-12 23:38:27 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
           {
             protocol: 'https',
-            hostname: 'www.m3xtrader.com',
+            hostname: 'm3xtrader.com',
           },
         ],
       },
